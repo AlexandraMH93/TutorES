9a9e74a66c91b649a5f81e9049a291c6ab7a3a90
Yoel Betancor <juyobema@gmail.com>
2024-04-05 13:44:25 +0100

    changed styles

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -29,17 +29,13 @@ const router = createBrowserRouter([
              {
                  path:'',
                  element: <TeacherCalendar/>
-             }
-              
-        ]
-        path:'teacher',
-        element: <MainLayout />,
-        children:[
-            {
+             },
+             {
                 path:'subjects',
                 element: <TeacherSubjects />
             }
-        ]
+              
+        ] 
     },
     {
         path:'student',
