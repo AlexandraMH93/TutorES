34c8b2332b2cd1ab94331a66b48e50bf573668d3
Musa Djiguel <musadjiguel@gmail.com>
2024-04-08 10:10:52 +0100

    added elements

--- src/pages/TeacherClasses/TeacherClasses.jsx
+++ src/pages/TeacherClasses/TeacherClasses.jsx
@@ -1,52 +1,152 @@
-import { Box, Button, Typography  } from '@mui/material'
-import './TeacherClasses.css' 
-import { Link } from 'react-router-dom'
+import { Card, CardContent, Typography, Box } from "@mui/material"
+import { Link } from "react-router-dom"
+import './TeacherClasses.css'
 
-const TeacherClasses = () => {
+const Class = () => {
+
+
   return (
-    <div id="mainContainer">
-      <Typography variant="h2">Your next class</Typography>
+    <div className="class">
+       <Card>
+        <CardContent>
+            <Typography variant="h5" mt={10}ml={10} >
+              Your next Class
+            </Typography>
+            
+            <div id="box1">
+            <Typography variant="body2" color="text.secondary" ml={10}>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste repellat alias eum natus, quibusdam atque officiis debitis temporibus dignissimos commodi dicta provident sunt reiciendis ipsam sapiente, dolorum maiores minima impedit!
+         Veniam nostrum laboriosam optio laudantium quos debitis pariatur quam nisi minima. Modi magni aut, consequatur maxime aliquid quidem delectus. Animi facere aliquid repellat perspiciatis officiis aut saepe accusantium explicabo quisquam?
+         Culpa dignissimos, officia incidunt earum maiores porro, possimus expedita exercitationem similique consequuntur rem, ratione ullam. Enim dolor voluptas nihil repudiandae consequatur? Optio cumque velit maxime beatae animi asperiores ullam natus?
+        </Typography>
 
-        <Box id='firstbox'
-            height={200}
-            width={600}
-            my={4}
-            display="flex"
-            alignItems="center"
-            gap={4}
-            p={2}
-            sx={{ border: '2px solid grey' }}
-            >
-                
-                    <div className='boximg'>
-                        <Typography variant="h6"> Foto perfil </Typography> 
-                    </div>
+            <div id="button">
+                <Link to=''>
+                Contact Teacher
+                </Link>
+                <button className="cancel"> Cancel date </button>
+            </div>
+        </div>
 
-                <div className='boxDateSubjet' >
-                    <div className='date'>
-                        <Typography variant="h6"> Date: </Typography>
-                    </div>
-                    <div className='subject'>
-                        <Typography variant="h6"> Subject: </Typography>
-                    </div>
-                </div>
-            <div>
-                <Typography type='body'>
-                    Comment: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia esse, quae unde natus in ipsum...
-                </Typography>
-            </div>
-            <div>
-                <Link href="#" underline="none">
-                {'underline="none"'}
-            </Link>
-            </div>
-            <div>
-                <Button variant="contained">Cancel Date </Button>
-            </div>
-        </Box>
-   
+            <Typography variant="h5"ml={8}>
+              Your other Classes
+            </Typography>
+            <input type="search" /> 
+            <input type="date" />
+
+        <div id="box2">
+            <Box
+                ml={8}
+                height={100}
+                width={100}
+                my={4}
+                display="flex"
+                alignItems="center"
+                gap={4}
+                p={2}
+                sx={{ border: '2px solid grey' }}
+              >
+            <img></img>
+            <Typography>
+              Alejandro
+              Data:<data> 04-04-24</data>
+            </Typography>
+              </Box>
+
+              <Box
+                ml={8}
+                height={100}
+                width={100}
+                my={4}
+                display="flex"
+                alignItems="center"
+                gap={4}
+                p={2}
+                sx={{ border: '2px solid grey' }}
+              >
+              <img></img>
+              <Typography>
+                Alejandro
+                Data: <data> 04-04-24</data>
+              </Typography>
+            </Box>
+
+            <Box 
+                ml={8}
+                height={100}
+                width={100}
+                my={4}
+                display="flex"
+                alignItems="center"
+                gap={4}
+                p={2}
+                sx={{ border: '2px solid grey' }}
+              >
+              <img></img>
+              <Typography>
+                Alejandro
+                Data: <data > 04-04-24</data>
+              </Typography>
+            </Box>
+
+            <Box 
+                ml={8}
+                height={100}
+                width={100}
+                my={4}
+                display="flex"
+                alignItems="center"
+                gap={4}
+                p={2}
+                sx={{ border: '2px solid grey' }}
+              >
+              <img></img>
+              <Typography>
+                Alejandro
+                Data: <data > 04-04-24</data>
+              </Typography>
+            </Box>
+
+            <Box 
+                ml={8}
+                height={100}
+                width={100}
+                my={4}
+                display="flex"
+                alignItems="center"
+                gap={4}
+                p={2}
+                sx={{ border: '2px solid grey' }}
+              >
+              <img></img>
+              <Typography>
+                Alejandro
+                Data: <data > 04-04-24</data>
+              </Typography>
+            </Box>
+
+            <Box 
+                ml={8}
+                height={100}
+                width={100}
+                my={4}
+                display="flex"
+                alignItems="center"
+                gap={4}
+                p={2}
+                sx={{ border: '2px solid grey' }}
+              >
+              <img></img>
+              <Typography>
+                Alejandro
+                Data: <data > 04-04-24</data>
+              </Typography>
+            </Box>
+        </div>
+        </CardContent>
+       </Card>
     </div>
   )
 }
 
-export default TeacherClasses
+export default Class
