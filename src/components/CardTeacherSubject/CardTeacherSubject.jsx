e3059c98ebcf7d1b9af60566dfbfa81b6354adec
Alexandra Mejías Herrera <alexandramh1001@gmail.com>
2024-04-09 16:08:05 +0100

    hover style in progress

--- src/components/CardTeacherSubject/CardTeacherSubject.jsx
+++ src/components/CardTeacherSubject/CardTeacherSubject.jsx
@@ -4,15 +4,18 @@ import './CardTeacherSubject.css'
 export const CardTeacherSubject = ({subjectName, subjectId, subjectImgUrl, onDelete}) => { /* hay que cargar las img desde la base de datos */
 
   return (
-    <Card className="subject">
+    <Card className='subject'>
       {" "}
       {/* defino la card de la subject del teacher */}
-      <CardContent
+      <CardContent 
+        className='subject-card'
         sx={{
           display: "flex",
           height: "100px",
+          width: '100px',
           flexDirection: "column",
           alignItems: "center",
+          gap: '10px'
         }}
       >
         <Typography>{subjectName}</Typography>
