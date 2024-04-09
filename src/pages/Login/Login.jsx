44f0ffbfddff16ea395ee2cad711b2312f8e485d
Musa Djiguel <musadjiguel@gmail.com>
2024-04-09 15:33:18 +0100

    done signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,68 +1,107 @@
-import { Button, Card, CardActions, CardContent, CardHeader, Icon, IconButton, InputAdornment, TextField } from "@mui/material"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { login } from "../../services/authService"
-import { Email, Lock, VisibilityOff } from "@mui/icons-material"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Icon,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../services/authService";
+import { Email, Lock, VisibilityOff } from "@mui/icons-material";
+import EmailIcon from '@mui/icons-material/Email';
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        const res = await login({email, password})
-        localStorage.setItem('token', res.token)
-        localStorage.setItem('role', res.role)
-        res.role == 'student' ? navigate('/student') : navigate('/teacher')
-    }
+  const handleLogin = async () => {
+    const res = await login({ email, password });
+    localStorage.setItem("token", res.token);
+    localStorage.setItem("role", res.role);
+    res.role == "student" ? navigate("/student") : navigate("/teacher");
+  };
+  const handleSignup = async () => {
+    navigate("/signUp");
+  };
+  return (
+    <>
+      <div className="wrapper"></div>
+      <div className="auth">
+        <Card>
+          <CardHeader
+            title="Login"
+            sx={{ display: "flex", textAlign: "center" }}
+          ></CardHeader>
 
-    return (
-        <>
-        <div className="wrapper"></div>
-        <div className="auth">
-            <Card >
-                <CardHeader title='Login' sx={{display: 'flex', textAlign: 'center'}} ></CardHeader>
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "end",
+              gap: "1em",
+            }}
+          >
+            <TextField
+              type="email"
+              variant="outlined"
+              label="Email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon>
+                      <EmailIcon />
+                    </Icon>
+                  </InputAdornment>
+                ),
+              }}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth={true}
+            />
+            <TextField
+              type="password"
+              variant="outlined"
+              label="Password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon>
+                      <Lock />
+                    </Icon>
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton>
+                      <VisibilityOff />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </CardContent>
 
-                <CardContent sx={{display: 'flex', flexDirection: 'column', justifyContent: 'end', gap: '1em'}} > 
-                    <TextField type='email' variant="outlined" label='Email'  
-                    InputProps={{
-                        startAdornment: (
-                        <InputAdornment>
-                            <Icon>
-                                 <Email />
-                            </Icon>
-                        </InputAdornment>
-                        )
-                    }}
-                     onChange={(e) => setEmail(e.target.value)} ></TextField>
-                    <TextField type='password' variant="outlined" label='Password' 
-                       InputProps={{
-                        startAdornment: (
-                        <InputAdornment>
-                            <Icon>
-                                <Lock />
-                            </Icon>
-                        </InputAdornment>
-                        ),
-                        endAdornment: (
-                        <InputAdornment>
-                            <IconButton>
-                                <VisibilityOff />
-                            </IconButton>
-                        </InputAdornment>
-                        )
-                    }}
-                    onChange={(e) => setPassword(e.target.value)}></TextField>
-                </CardContent>
-
-                <CardActions sx={{display: 'flex', justifyContent: 'end'}}>
-                    <Button color="success" onClick={() => {handleLogin()}}>Login</Button>
-                    <Button > Registrarse</Button>
-                </CardActions>
-            </Card>
-        </div>
+          <CardActions sx={{ display: "flex", justifyContent: "end" }}>
+            <Button
+              color="success"
+              onClick={() => {
+                handleLogin();
+              }}
+            >
+              Login
+            </Button>
+            <Button onClick={handleSignup}> Registrarse</Button>
+          </CardActions>
+        </Card>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,115 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  TextField,
+  Typography,
+  InputAdornment,
+  Icon
+} from "@mui/material";
+import { useState } from "react";
+import { signUp } from "../../services/authService";
+import { Link, useNavigate } from "react-router-dom";
+import EmailIcon from '@mui/icons-material/Email';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const SignUp = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-export default SignUp
+  const handleSignup = async () => {
+    const res = await signUp({
+      email,
+      name: { firstname, lastname },
+      username,
+      password,
+    });
+    localStorage.setItem("token", res.token);
+    localStorage.setItem("role", res.role);
+    navigate("/login");
+  };
+
+  return (
+    <div className="main">
+      <Card sx={{ width: "25vw" }}>
+        <CardHeader title="Sign up">
+          <Typography variant="h3">Sign up</Typography>
+        </CardHeader>
+        <CardContent
+          sx={{ display: "flex", flexDirection: "column", gap: "1em" }}
+        >
+          <TextField
+            type="text"
+            variant="filled"
+            label="Firstname"
+            onChange={(e) => setFirstname(e.target.value)}
+          ></TextField>
+          <TextField
+            type="text"
+            variant="filled"
+            label="Lastname"
+            onChange={(e) => setLastname(e.target.value)}
+          ></TextField>
+          <TextField
+            type="email"
+            variant="filled"
+            label="Email"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Icon>
+                    <EmailIcon />
+                  </Icon>
+                </InputAdornment>
+              ),
+            }}
+            onChange={(e) => setEmail(e.target.value)}
+          ></TextField>
+          <TextField
+            type="text"
+            variant="filled"
+            label="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          ></TextField>
+          <TextField
+            type="password"
+            variant="filled"
+            label="Password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Icon>
+                    <VisibilityOffIcon />
+                  </Icon>
+                </InputAdornment>
+              ),endAdornment: (
+                <InputAdornment position="end">
+                  <Icon>
+                    <VisibilityOffIcon />
+                  </Icon>
+                </InputAdornment>
+              ),
+            }}
+            onChange={(e) => setPassword(e.target.value)}
+          ></TextField>
+        </CardContent>
+        <CardActions sx={{ display: "flex", justifyContent: "end" }}>
+          <Link to={"/"}>
+            <Button>Cancel</Button>
+          </Link>
+          <Button variant="contained" onClick={() => handleSignup()}>
+            Sign up
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
