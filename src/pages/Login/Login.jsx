13c19de75ac25a1d6bbbeee6cd1b7abf974ab2d3
Yoel Betancor <juyobema@gmail.com>
2024-04-11 13:50:48 +0100

    Changed login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { Button, Card, CardActions, CardContent, CardHeader, Icon, IconButton, InputAdornment, TextField, Typography, Divider } from "@mui/material"
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { login } from "../../services/authService"
 import { Email, Lock, VisibilityOff } from "@mui/icons-material"
 
@@ -22,7 +22,7 @@ const Login = () => {
         <div className="wrapper"></div>
         <div className="auth">
             <Card >
-                <CardHeader title='Login' sx={{display: 'flex', textAlign: 'center'}} ></CardHeader>
+                <CardHeader title='Iniciar sesión' sx={{display: 'flex', textAlign: 'center'}} ></CardHeader>
 
                 <CardContent sx={{display: 'flex', flexDirection: 'column', justifyContent: 'end', gap: '1em'}} > 
                     <TextField type='email' variant="outlined" label='Email'  
@@ -59,8 +59,8 @@ const Login = () => {
                 <Divider />
 
                 <CardActions sx={{display: 'flex', flexDirection: 'column',justifyContent: 'center'}}>
-                    <Button variant="outlined" color="success" onClick={() => {handleLogin()}} fullWidth>Login</Button>
-                    <Typography>Don't have account?<Button > Registrarse</Button></Typography>
+                    <Button variant="outlined" color="primary" onClick={() => {handleLogin()}} fullWidth>Login</Button>
+                    <Typography>¿No tienes cuenta? <Link to="/signup"><Button color="primary" > Registrate</Button></Link></Typography>
                     
                 </CardActions>
             </Card>
